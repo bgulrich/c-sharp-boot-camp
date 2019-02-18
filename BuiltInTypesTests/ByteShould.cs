92027ebd6a22e64f1c750da51f339cdffd2beb31
Sudarsan Pandey <s0p01qr@HOMEOFFICE.wal-mart.com>
2019-02-18 11:46:09 -0600

    HomeWork

---
+++ BuiltInTypesTests/ByteShould.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BuiltInTypesTests
+{
+    public class ByteShould
+    {
+        [Theory]
+        [InlineData(20,20)]
+        [InlineData(40,40)]
+        [InlineData(202,202)]
+        [InlineData(255,255)]
+        public void BeSameAsUnsigned8BitInteger(byte b, int i)
+        {
+            Assert.Equal(b, i);
+        }
+
+        [Theory]
+        [InlineData(255,1,0)]
+        [InlineData(5,255,4)]
+        [InlineData(byte.MaxValue,byte.MaxValue,254)]
+        public void RecountFromZeroWhenMoreThan255(byte b, byte b1, byte expected)
+        {
+            Assert.Equal(expected,(byte)(b+b1));
+        }
+
+        [Fact]
+        public void ThrowOverflowExceptionIfMoreThan255()
+        {
+            int number = Int32.MaxValue;
+            Assert.Throws<OverflowException>(()=>Convert.ToByte(number));
+        }
+
+    }
+}
---
+++ BuiltInTypesTests/LongShould.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BuiltInTypesTests
+{
+    public class LongShould
+    {
+        [Fact]
+        public void ThrowExceptionIfExceedsTheRange()
+        {
+            long l = long.MaxValue;
+            string lStr = l + "1"; //This will make the string out of the range of long
+            Assert.Throws<OverflowException>(() => long.Parse(lStr));
+        }
+        [Fact]
+        public void BeASigned64BitInteger()
+        {
+            Assert.Equal(typeof(long), typeof(Int64));
+        }
+        [Fact]
+        public void ImplicitlyConvertFromHex()
+        {
+            var hex = 0x1000000;
+            long l = hex;
+            Assert.Equal(hex, l);
+        }
+    }
+}
