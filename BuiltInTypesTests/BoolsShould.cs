7867dab0ffa41b97ef592fa7b1c55902e8c9d3a6
Tianyi Zhang <t0z00fx@homeoffice.wal-mart.com>
2019-02-18 22:18:18 -0600

    Submitting homework 3 out of 3. Testing on bools.

--- BuiltInTypesTests/BoolsShould.cs
+++ BuiltInTypesTests/BoolsShould.cs
@@ -32,5 +32,48 @@ namespace BuiltInTypesTests
             Assert.Equal("True", bool.TrueString);
             Assert.Equal("False", bool.FalseString);
         }
+
+        [Fact]
+        public void BeAutoRecognizedWhenAssignedTrueOrFalseValue()
+        {
+            var unknownType = true;
+
+            Assert.Equal("System.Boolean", unknownType.GetType().ToString());
+        }
+
+        [Fact]
+        public void BeAssignedThroughDifferentApproach()
+        {
+            bool myBool_1 = 5 > 6;
+            bool myBool_2 = 3 + 4 == 8;
+            bool myBool_3 = false;
+
+            Assert.False(myBool_1);
+            Assert.False(myBool_2);
+            Assert.False(myBool_3);
+        }
+
+        [Fact]
+        public void BeCastedToLargerTypes()
+        {
+            var myBool = true;
+            byte myByte = Convert.ToByte(myBool);
+            int myInt = Convert.ToInt32(myBool);
+            double myDouble = Convert.ToDouble(myBool);
+
+            Assert.Equal(1, myByte);
+            Assert.Equal(1, myInt);
+            Assert.Equal(1, myDouble);
+        }
+
+        [Fact]
+        public void HasTheSmallestSize()
+        {
+            Assert.True(sizeof(Boolean) < sizeof(Char));
+            Assert.True(sizeof(Char) < sizeof(Int32));
+            Assert.True(sizeof(Int32) < sizeof(Double));
+            Assert.True(sizeof(Double) < sizeof(Decimal));
+            Assert.Equal(1, sizeof(Boolean));
+        }
     }
 }
